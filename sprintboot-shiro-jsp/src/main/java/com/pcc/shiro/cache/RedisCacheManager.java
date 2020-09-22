024822ca561a4457c83dda455c21634bf7e317ce
彭聪 <1058246935@qq.com>
2020-09-22 21:00:11 +0800

    首次提交

---
+++ sprintboot-shiro-jsp/src/main/java/com/pcc/shiro/cache/RedisCacheManager.java
@@ -0,0 +1,16 @@
+package com.pcc.shiro.cache;
+
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+import org.apache.shiro.cache.CacheManager;
+
+/**
+ * @author : 彭聪
+ * @date : 2020-09-22 20:56
+ **/
+public class RedisCacheManager implements CacheManager {
+    @Override
+    public <K, V> Cache<K, V> getCache(String s) throws CacheException {
+        return null;
+    }
+}
