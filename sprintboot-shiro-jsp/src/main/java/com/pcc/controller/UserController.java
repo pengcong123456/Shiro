94f9ef28bfadf39d806b5f0ecc12f6c282cfbfcf
彭聪 <1058246935@qq.com>
2020-09-23 13:05:22 +0800

    验证码完成

--- sprintboot-shiro-jsp/src/main/java/com/pcc/controller/UserController.java
+++ sprintboot-shiro-jsp/src/main/java/com/pcc/controller/UserController.java
@@ -2,6 +2,8 @@ package com.pcc.controller;
 
 import com.pcc.entity.User;
 import com.pcc.service.UserService;
+import com.pcc.utils.VerifyCodeUtils;
+import com.sun.deploy.net.HttpResponse;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
@@ -13,7 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * @author : 彭聪
@@ -27,7 +32,7 @@ public class UserController {
     private UserService userService;
 
 
-    @RequiresRoles(value = {"admin","user"})
+    @RequiresRoles(value = {"admin", "user"})
     @RequiresPermissions("user:*:*")
     @RequestMapping("save")
     public String save() {
@@ -36,8 +41,15 @@ public class UserController {
     }
 
     @RequestMapping("getImage")
-    public void getImage(){
-
+    public void getImage(HttpSession session, HttpServletResponse response) throws IOException {
+        //生成验证码
+        String code = VerifyCodeUtils.generateVerifyCode(4);
+        //验证码放入session
+        session.setAttribute("code", code);
+        //验证码存入图片
+        ServletOutputStream os = response.getOutputStream();
+        response.setContentType("image/png");
+        VerifyCodeUtils.outputImage(220, 60, os, code);
     }
 
     @RequestMapping("register")
@@ -61,11 +73,19 @@ public class UserController {
 
 
     @RequestMapping("login")
-    public String login(String username, String password, HttpSession session) {
-        Subject subject = SecurityUtils.getSubject();
+    public String login(String username, String password, String code, HttpSession session) {
+
+        String trueCode = (String) session.getAttribute("code");
         try {
-            subject.login(new UsernamePasswordToken(username, password));
-            return "redirect:/index.jsp";
+            if (trueCode.equalsIgnoreCase(code)) {
+                Subject subject = SecurityUtils.getSubject();
+                subject.login(new UsernamePasswordToken(username, password));
+                return "redirect:/index.jsp";
+            }else {
+                new RuntimeException("验证码错误");
+                System.out.println("验证码错误");
+            }
+
         } catch (UnknownAccountException e) {
             e.printStackTrace();
             System.out.println("用户名错误");
